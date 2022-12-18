31a36436f0b6b3bd6f7a6d921bd4b70ff3177508
PillowPowa <97808006+PillowPowa@users.noreply.github.com>
2022-12-18 23:27:26 +0200

    ✏️ Examples update

--- examples/cat.js
+++ examples/cat.js
@@ -1,37 +1,31 @@
+const { createCanvas } = require("canvas");
 const { writeFile } = require("fs");
-const { Encoder, Decoder } = require("../dist");
+const { GIF } = require("../dist");
 
-const decoder = new Decoder()
-  // .setUrl("https://media3.giphy.com/media/3oz8xsaLjLVqVXr3tS/200.gif")
-  .setUrl(__dirname + "\\cat_default.gif") // U also can set path to your gif
+const gif = new GIF().setQuality(100);
 
-// Method "start" returns Promise, so..
-decoder.start().then((frameData) => {
-  const { width, height, delay } = frameData[0].details; // Get encoded gif width, height and delay, easy :3
-
-  const encoder = new Encoder(width, height)
-    .setDelay(delay) // set encoded gif delay
-    .setQuality(100) // set gif quality (affects colors)
-    .start(); // required method "start" returns encoder
-
+gif.decode(__dirname + "\\cat_default.gif", 20).then((frameData) => {
+  console.log(frameData.length)
   let font = 10;
 
-  const context = encoder.getContext();
+  const canvas = createCanvas(gif.width, gif.height);
+  const context = canvas.getContext("2d");
+
   context.fillStyle = "#000";
   context.textAlign = "center";
   context.font = `${font}px Sans`;
 
   for (let i = 0; i < frameData.length; i++) {
-    context.drawImage(frameData[i].getImage(), 0, 0, width, height); // draw Frame #i with encoded gif width&height
+    context.drawImage(frameData[i].getImage(), 0, 0, gif.width, gif.height); // draw Frame #i with encoded gif width&height
     context.font = `${font++}px Sans`;
-    context.fillText("Cats rule the world", width / 2, 100)
-    context.fillText(`${i}/${frameData.length}`, width / 2, height / 2 + 50);
-    encoder.updateFrame(); // Add a new frame to ur custom gif =) 
+    context.fillText("Cats rule the world", gif.width / 2, 100)
+    context.fillText(`${i}/${frameData.length}`, gif.width / 2, gif.height / 2 + 50);
+    gif.addFrame(context); // Add a new frame to ur custom gif =) 
   }
 
   writeFile(
     __dirname + "\\cat.gif", // path where u want save the gif
-    encoder.finish(), // method "finish" returns buffer
+    gif.buffer(), // method "finish" returns buffer
     () => console.log("Encode ended!")
   );
-});
+})
